1540554d6c9f16bc3fed2718a37d21cd13d7935e
PyroTrinity <masavero2310@gmail.com>
2025-07-09 18:58:02 +0700

    refactor: Remove NavMenu component and update Menu page to use Header and Footer

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import Login from "../components/Login";
 import Header from "../components/Header";
 import Services from "../components/Services";
+import Footer from "../components/Footer";
 
 export default function Main() {
   return (
@@ -8,6 +9,7 @@ export default function Main() {
       <Header/>
       <Login/>
       <Services/>
+      <Footer/>
       </>
   )
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,13 +1,15 @@
 import MenuItem from "../components/Menu";
-import NavMenu from "../components/NavMenu";
+import Header from "../components/Header";
 import Services from "../components/Services";
+import Footer from "../components/Footer";
 
 export default function Menu() {
   return (
     <>
-    <NavMenu/>
+    <Header/>
     <MenuItem/>
     <Services/>
+    <Footer/>
     </>
   )
 }
