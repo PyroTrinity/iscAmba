fb09b47e90cefb1230937925b49ea22fe53b65f6
PyroTrinity <masavero2310@gmail.com>
2025-07-09 19:35:24 +0700

    refactor: Rename Menu component to MenuPage for clarity and remove unused HomePage import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './App.css'
-import HomePage from './pages/HomePage'
 import Menu from './pages/Menu'
 import Login from './pages/Login'
 import Header from './components/Header'
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,7 +2,7 @@ import Menu from "../components/Menu";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function Menu() {
+export default function MenuPage() {
   return (
     <>
     <Header/>
